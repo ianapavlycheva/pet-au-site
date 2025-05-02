797eea56d2ce087d00d7754c82beb02ee2989274
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2025-05-02 19:18:46 +1000

    Publish sanity and react app

--- pet-hub-studio/sanity.cli.js
+++ pet-hub-studio/sanity.cli.js
@@ -3,11 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'r02fqaop',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
+  studioHost: 'pet-hub-studio',
   autoUpdates: true,
 })
