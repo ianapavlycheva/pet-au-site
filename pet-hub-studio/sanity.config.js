a72af968c9c1f695591f53fe7715db1cdf8b8415
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2025-05-02 19:42:27 +1000

    test1

--- pet-hub-studio/sanity.config.js
+++ pet-hub-studio/sanity.config.js
@@ -1,3 +1,33 @@
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+// import {defineConfig} from 'sanity'
+// import {structureTool} from 'sanity/structure'
+// import {visionTool} from '@sanity/vision'
+// import {schemaTypes} from './schemaTypes'
+
+// export default defineConfig({
+//   name: 'default',
+//   title: 'pet-hub-studio',
+
+//   // projectId: 'r02fqaop',
+//   // dataset: 'production',
+
+//   projectId: process.env.SANITY_PROJECT_ID,
+//   dataset: process.env.SANITY_DATASET,
+
+//   plugins: [structureTool(), visionTool()],
+
+//   schema: {
+//     types: schemaTypes,
+//   },
+// })
+
+import dotenv from 'dotenv'
+
+// Load environment variables from the .env file
+dotenv.config()
+
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
@@ -7,8 +37,9 @@ export default defineConfig({
   name: 'default',
   title: 'pet-hub-studio',
 
-  projectId: 'r02fqaop',
-  dataset: 'production',
+  // Access project ID and dataset from environment variables
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
 
   plugins: [structureTool(), visionTool()],
 
@@ -16,3 +47,29 @@ export default defineConfig({
     types: schemaTypes,
   },
 })
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+// import {defineConfig} from 'sanity'
+// import {structureTool} from 'sanity/structure'
+// import {visionTool} from '@sanity/vision'
+// import {schemaTypes} from './schemaTypes'
+
+// export default defineConfig({
+//   name: 'default',
+//   title: 'pet-hub-studio',
+
+//   projectId: process.env.SANITY_PROJECT_ID,
+//   dataset: process.env.SANITY_DATASET,
+
+//   // Example of using the API token
+//   api: {
+//     token: process.env.SANITY_API_TOKEN, // Use the token for authenticated requests
+//   },
+
+//   plugins: [structureTool(), visionTool()],
+
+//   schema: {
+//     types: schemaTypes,
+//   },
+// })
