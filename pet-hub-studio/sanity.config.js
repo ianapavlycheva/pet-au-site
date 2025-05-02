5a943ded61829db3ed731ade88b674ec8405bf01
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2025-05-02 19:45:33 +1000

    test2

--- pet-hub-studio/sanity.config.js
+++ pet-hub-studio/sanity.config.js
@@ -1,28 +1,3 @@
-// import dotenv from 'dotenv'
-// dotenv.config()
-
-// import {defineConfig} from 'sanity'
-// import {structureTool} from 'sanity/structure'
-// import {visionTool} from '@sanity/vision'
-// import {schemaTypes} from './schemaTypes'
-
-// export default defineConfig({
-//   name: 'default',
-//   title: 'pet-hub-studio',
-
-//   // projectId: 'r02fqaop',
-//   // dataset: 'production',
-
-//   projectId: process.env.SANITY_PROJECT_ID,
-//   dataset: process.env.SANITY_DATASET,
-
-//   plugins: [structureTool(), visionTool()],
-
-//   schema: {
-//     types: schemaTypes,
-//   },
-// })
-
 import dotenv from 'dotenv'
 
 // Load environment variables from the .env file
@@ -47,29 +22,3 @@ export default defineConfig({
     types: schemaTypes,
   },
 })
-// import dotenv from 'dotenv'
-// dotenv.config()
-
-// import {defineConfig} from 'sanity'
-// import {structureTool} from 'sanity/structure'
-// import {visionTool} from '@sanity/vision'
-// import {schemaTypes} from './schemaTypes'
-
-// export default defineConfig({
-//   name: 'default',
-//   title: 'pet-hub-studio',
-
-//   projectId: process.env.SANITY_PROJECT_ID,
-//   dataset: process.env.SANITY_DATASET,
-
-//   // Example of using the API token
-//   api: {
-//     token: process.env.SANITY_API_TOKEN, // Use the token for authenticated requests
-//   },
-
-//   plugins: [structureTool(), visionTool()],
-
-//   schema: {
-//     types: schemaTypes,
-//   },
-// })
